55268b780417c8eff3034852c8654995c8a3b162
carolina <you@example.com>
2020-02-18 23:36:41 -0300

    shuffle deal and contains methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,10 +1,23 @@
 defmodule Cards do
   def createDeck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+
+    for suit <- suits, value <-values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, handSize) do
+    Enum.split(deck, handSize)
   end
 
 end
