dfef620d61d1ecd55f462fae7353c8cef13ac0c7
carolina <you@example.com>
2020-02-23 11:16:18 -0300

    Notes on pattern matching and pipe operator

--- lib/cards.ex
+++ lib/cards.ex
@@ -20,4 +20,30 @@ defmodule Cards do
     Enum.split(deck, handSize)
   end
 
+  def save(deck, fileName) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(fileName, binary)
+  end
+
+  def load(fileName) do
+    # Reminds me of the the 'when' statement in Kotlin
+    # :ok -> primitive data type called atom, for status code. Atoms can be
+    # understood as strings, but are not displayed for the user. They're only used for
+    # status checking
+    case File.read(fileName) do
+      {:ok, binary} -> :erlang.binary_to_term binary # pattern matching can be used as a case statement for comparison and assignment
+      {:error, _reason} -> "That file does not exist."
+    end
+  end
+
+  def createHand(handSize) do
+    # deck = Cards.createDeck
+    # deck = Cards.shuffle(deck)
+    # hand = Cards.deal(deck,handSize)
+
+    # The pipe operator performs point-free style operations like JavaScript currying.
+    Cards.createDeck
+    |> Cards.shuffle
+    |> Cards.deal(handSize)
+  end
 end
